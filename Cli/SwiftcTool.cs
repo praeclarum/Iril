b6f40c45697f5cd3ce0198519be16462f336457b
Frank A. Krueger <fak@praeclarum.org>
2019-03-05 18:58:16 -0800

    Start work on Swift support

--- Cli/SwiftcTool.cs
+++ Cli/SwiftcTool.cs
@@ -5,12 +5,10 @@ using System.IO;
 
 namespace Cli
 {
-    public class ClangTool : CommandLineTool
+    public class SwiftcTool : CommandLineTool
     {
         public override HashSet<string> InputExtensions { get; } = new HashSet<string> {
-            ".c",
-            ".cpp",
-            ".ino"
+            ".swift",
         };
 
         public override IEnumerable<string> Run (IEnumerable<string> inputFiles)
@@ -22,16 +20,16 @@ namespace Cli
             var buildDir = Path.GetTempPath ();
 
             var args = new List<string> {
-                "-g", "-O1", "-S", "-emit-llvm", "-fpic"
+                "-emit-ir", "-g", "-Osize"
             };
             args.AddRange (files);
 
             var outFiles = new List<string> ();
-            if (Run (buildDir, "clang", args.ToArray ()) == 0) {
+            if (Run (buildDir, "swiftc", args.ToArray ()) == 0) {
                 foreach (var f in files) {
                     var outName = Path.ChangeExtension (Path.GetFileName (f), ".ll");
                     var outPath = Path.Combine (buildDir, outName);
-                    //Console.WriteLine (outPath);
+                    Console.WriteLine (outPath);
                     outFiles.Add (outPath);
                 }
             }
