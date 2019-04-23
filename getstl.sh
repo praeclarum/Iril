aa12a5e481fba245f1ce233a399a4c81e2f2b65c
Frank A. Krueger <fak@praeclarum.org>
2019-04-22 21:03:04 -0700

    Add libcxx

---
+++ getstl.sh
@@ -0,0 +1,19 @@
+set -e
+set -x
+
+if [ ! -f ${TMPDIR}stl.zip ]; then
+    wget -O ${TMPDIR}stl.zip https://github.com/llvm/llvm-project/archive/release/8.x.zip
+fi
+
+unzip -n ${TMPDIR}stl.zip  -x /clang-tools-extra/* clang compiler-rt debuginfo-tests libclc libcxxabi libunwind polly llvm -d ${TMPDIR}stlwork
+
+pushd ${TMPDIR}stlwork/llvm-project-release-8.x/libcxx/src
+
+clang -g -O1 -S -emit-llvm -std=c++17 -frtti -D_LIBCPP_BUILDING_LIBRARY *.cpp
+
+zip libcxx.zip *.ll
+
+popd
+
+mkdir -p ./Iril/Lib
+cp ${TMPDIR}stlwork/llvm-project-release-8.x/libcxx/src/libcxx.zip ./Iril/Lib/
