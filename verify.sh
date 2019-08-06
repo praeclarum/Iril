181656965328d2f1af5459d495c94e7277d0496d
Frank A. Krueger <fak@praeclarum.org>
2019-08-05 17:12:21 -0700

    Add IL verification tool

---
+++ verify.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+dotnet Lib/ILVerify/ILVerify.dll $1 -r ~/.nuget/packages/netstandard.library/2.0.0/build/netstandard2.0/ref/mscorlib.dll -r ~/.nuget/packages/netstandard.library/2.0.0/build/netstandard2.0/ref/netstandard.dll
