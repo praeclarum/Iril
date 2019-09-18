8e382fb85542a3c2250f88a5c258c02949da9ab6
Frank A. Krueger <fak@praeclarum.org>
2019-09-18 16:06:19 -0700

    Scaffold snprintf

---
+++ StdLib/IO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace StdLib
+{
+    [DllExport]
+    public static class IO
+    {
+        [DllExport("@__snprintf_chk")]
+        public static unsafe int snprintf_chk (byte* s, long maxlen, int flags, long len, byte* format, params object[] arguments)
+        {
+            var args = stackalloc __va_list_tag[1];
+            Llvm.va_start ((byte*)args, arguments);
+            Llvm.va_end ((byte*)args);
+            throw new NotImplementedException ();
+        }
+    }
+}
